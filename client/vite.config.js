87ea0f4f1e5b29a381a6e3ff6036e1d16096c8d2
FelixOgero <ogerofelix2001@gmail.com>
2025-09-22 13:30:18 +0300

    further changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        // target: "http://localhost:5000",
-        target: "https://paypal-payment-api-integration.onrender.com",
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
